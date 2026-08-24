3442e7ec3ec4816664d671efbb5a16f3171cffa0
Milton <miltonsarkissian@hotmail.com>
2026-08-24 19:21:22 -0300

    primer test: inicializaEnPosicionX

---
+++ TestMarsRover/RoverTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TestMarsRover
+{
+    [TestClass]
+    public sealed class RoverTests
+    {
+        [TestMethod]
+        public void InicializaEnLaPosicionXIndicada()
+        {
+            var rover = new Rover(0, 0, N);
+
+            var x = rover.x;
+
+            Assert.AreEqual(0, x);
+        }
+    }
+}
