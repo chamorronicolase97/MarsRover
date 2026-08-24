02d51e9da0c5514438823ad39e9336402a8ae48d
daniel-gagliano <daniel.gagliano3@gmail.com>
2026-08-24 19:34:16 -0300

    Test InicializaEnLaPosicionYIndicada-- En rojo

--- TestMarsRover/RoverTests.cs
+++ TestMarsRover/RoverTests.cs
@@ -13,6 +13,19 @@ namespace TestMarsRover
             var x = rover.PosicionX;
 
             Assert.AreEqual(0, x);
+        }        
+    
+    
+        [TestMethod]
+        public void InicializaEnLaPosicionYIndicada()
+        {
+            var rover = new Rover(0, 0, "N");
+
+            var y = rover.PosicionY;
+
+            Assert.AreEqual(0, y);
         }
+
     }
 }
+
